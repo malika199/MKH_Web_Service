31dbcb709f14ec771a5232c9f1f93e792e6bdc76
malika199 <malika.derfoufii@gmail.com>
2023-01-19 11:41:28 +0100

    create object

---
+++ data.js
@@ -0,0 +1,22 @@
+const books = [
+    { db1 },
+    { db2 },
+
+    { db3:[
+       { tab1: [
+        {id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald'},
+        {id: 2, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald'},
+        {id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald'}
+        ] ,
+        tab2: [
+            {id: 1, title: 'allo', author: 'F. Scott Fitzgerald'},
+            {id: 2, title: 'katy perry', author: 'F. Scott Fitzgerald'},
+            {id: 3, title: 'loulou katy', author: 'F. Scott Fitzgerald'}
+            ] }
+
+    ] 
+
+},
+    { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
+  ];
+  
--- server.js
+++ server.js
@@ -1,55 +1,267 @@
-const http = require('http');
-const url = require('url');
-const parse = require('parse-js');
+const http = require("http");
+const fs = require("fs");
+const data = require("./db.json");
 
-const books = [
-  { id: 1, title: 'The Old Man and the Sea', author: 'Ernest Hemingway' },
-  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-  { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
-];
+const port = 8000;
+const hostname = "localhost";
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
-  console.log(req.url)
-  const { pathname, query } = url.parse(url, true);
-  var path = req.url.split('?')[0];
+function setHeader(res, status) {
+  res.writeHead(status, { "Content-type": "application/json" });
+}
 
+// Helper used to send a message with a map of values
+function sendEnum(res, name, value) {
+  const keys = value?.map((el) => Object.keys(el)?.[0]);
+  res.end(
+    `{"message": "Please select one of the following ${name} : ${keys.map(
+      (el) => "\n/" + el
+    )}\n"}`
+  );
+}
 
-  if (method === 'GET' && pathname === '/books') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(books));
-  } else if (method === 'POST' && pathname === '/books') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-    req.on('end', () => {
-      const book = JSON.parse(body);
-      books.push(book);
-      res.end(JSON.stringify(book));
-    });
-  } else if (method === 'PUT' && pathname === '/books') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-    req.on('end', () => {
-      const book = JSON.parse(body);
-      const index = books.findIndex((b) => b.id === book.id);
-      books[index] = book;
-      res.end(JSON.stringify(book));
-    });
-  } else if (method === 'DELETE' && pathname === '/books') {
-    const id = parseInt(query.id);
-    const index = books.findIndex((b) => b.id === id);
-    books.splice(index, 1);
-    res.end();
+function writeFile(content) {
+  console.log("content pour write in file", content);
+  fs.writeFile("./db.json", JSON.stringify(content), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function isDefined(variable) {
+  console.log("variable", variable);
+  return variable != undefined;
+}
+
+function reqHandler(req, res) {
+  const { url, method } = req;
+
+  const isGetMethod = method === "GET";
+  const isPostMethod = method === "POST";
+  const isPutMethod = method === "PUT";
+  const isDeleteMethod = method === "DELETE";
+  console.log("url ::", url);
+  const urlSplited = url.split("/");
+
+  console.log("urlSplited", urlSplited);
+  const db = urlSplited[1];
+  console.log("db  ", db);
+
+  const selectedDb = data?.find((el) => Object.keys(el)[0] == db)?.[db] || [];
+
+  //  const selecteDTable =  selectedDb?.find((el) => Object.keys(el)[0] == tble)?.[tble] || [];
+
+  console.log(" selectedDb ", selectedDb);
+  const route = urlSplited[2];
+  console.log("route  ", route);
+
+  const selectedRoute =
+    selectedDb?.find((el) => Object.keys(el)?.[0] == route)?.[route] || [];
+
+  console.log("selectedRoute", selectedRoute);
+  const id = urlSplited[3];
+  const selectedProperty = selectedRoute?.find((el) => el.id == id);
+  console.log(" selectedProperty : ", selectedProperty);
+  /**
+   * Sequence of conditions
+   * No dataBase => GET all databases, POST create DB, PUT edit DB, DELETE delete DB (if DB name provided)
+   * database => check for table
+   *
+   * No table => GET all tables, POST create table, PUT edit table, DELETE delete tables (if table name provided)
+   * table => check for property
+   *
+   * No property id => GET all properties, POST create property, PUT edit property, DELETE delete property (if property id provided)
+   * property => send property infos
+   */
+  if (!db || (db == "/" && selectedDb.length == 0)) {
+    if (isGetMethod) {
+      setHeader(res, 404);
+      sendEnum(res, "DataBases", data);
+    } else if (isPostMethod) {
+      createDataBase(req, res);
+      setHeader(res, 200);
+      res.end('{ "message": "Database created successfully"}');
+    } else if (isPutMethod) {
+      updateDataBase(data, req, res);
+      setHeader(res, 200);
+      res.end('{ "message": "Database edited successfully"}');
+    } else if (isDeleteMethod) {
+      deleteDataBase(data, req, res);
+      setHeader(res, 200);
+      res.end('{ "message": "Database deleted successfully"}');
+    }
+  } else if (db.length !== 0 && isDefined(route) ) {
+    if (isPostMethod) {
+      createObject(db, route, req, res);
+      setHeader(res, 200);
+      res.end('{ "message": "Object created successfully"}');
+    } 
   } else {
-    res.statusCode = 404;
-    res.end();
+    console.log("route", route);
+    if ( db.length !== 0 && !isDefined(route) ) {
+      if (isGetMethod) {
+        setHeader(res, 200);
+        sendEnum(res, "Routes", selectedDb);
+      } else if (isPostMethod) {
+        createTable(db, req, res);
+        setHeader(res, 200);
+        res.end('{ "message": "Table created successfully"}');
+      } else if (isPutMethod) {
+        setHeader(res, 200);
+      } else if (isDeleteMethod) {
+          deleteTable(db, req, res)
+          setHeader(res, 200);
+          res.end('{ "message": "Table deleted successfully"}');
+       
+      }
+    } else {
+      if (isGetMethod) {
+        if (!id || id == "/" || !selectedProperty) {
+          setHeader(res, 200);
+          res.end(JSON.stringify(selectedRoute));
+        } else {
+          setHeader(res, 200);
+          res.end(JSON.stringify(selectedProperty));
+        }
+      } else if (isPostMethod) {
+        setHeader(res, 200);
+      } else if (isPutMethod) {
+        setHeader(res, 200);
+      } else if (isDeleteMethod) {
+        setHeader(res, 200);
+      }
+    }
   }
+}
+
+const server = http.createServer(reqHandler);
+server.listen(port, hostname, (err) => {
+  if (err) {
+    console.log("ERROR ⛔ :" + err);
+  }
+  console.log("SERVER is running 🔥 at port : " + port);
 });
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+function createDataBase(req, res) {
+  let body = [];
+  req.on("data", function (data) {
+    body.push(data);
+  });
+  req.on("end", function () {
+    let content;
+    body = JSON.parse(Buffer.concat(body).toString());
+    if (isDefined(body.name)) {
+      content = {
+        [body.name]: isDefined(body.tables) ? body.tables : [],
+      };
+      data.push(content);
+      writeFile(data);
+    } else {
+      setHeader(res, 404);
+      res.end('{ "message": "Must provide a database name"}');
+    }
+  });
+}
+
+function deleteDataBase(dataBase, req, res) {
+  let body = [];
+  req.on("data", function (data) {
+    body.push(data);
+  });
+  req.on("end", function () {
+    body = JSON.parse(Buffer.concat(body).toString());
+    if (isDefined(body.name)) {
+      const filtredDataBase = dataBase.filter(
+        (el) => Object.keys(el)[0] !== body.name
+      );
+      writeFile(filtredDataBase);
+    } else {
+      setHeader(res, 404);
+      res.end('{ "message": "Must provide a database name"}');
+    }
+  });
+}
+
+function updateDataBase(dataBase, req, res) {
+  deleteDataBase(dataBase, req, res);
+  createDataBase(req, res);
+}
+
+function createTable(dataBaseName, req, res) {
+  let body = [];
+  req.on("data", function (data) {
+    body.push(data);
+  });
+  req.on("end", function () {
+    let content;
+    body = JSON.parse(Buffer.concat(body).toString());
+    console.log("body  :  ", body);
+    if (isDefined(body.name)) {
+      content = {
+        [body.name]: isDefined(body.properties) ? body.properties : [],
+      };
+      console.log("bcontent :  ", body);
+
+      for (let i = 0; i < data.length; i++) {
+        console.log("data", data);
+        if (Object.keys(data[i])[0] === dataBaseName) {
+          data[i][dataBaseName].push(content);
+          console.log("dataBaseName", dataBaseName);
+        }
+      }
+      writeFile(data);
+    } else {
+      setHeader(res, 404);
+      res.end('{ "message": "Must provide a database name"}');
+    }
+  });
+}
+
+function createObject(dataBaseName, table, req, res) {
+  console.log("ooooooooooo", { dataBaseName, table });
+  let body = [];
+  req.on("data", function (data) {
+    body.push(data);
+  });
+  req.on("end", function () {
+    let BDD;
+    body = JSON.parse(Buffer.concat(body).toString());
+    if (isDefined(body)) {
+      data.map((el) => console.log("test ", el));
+      for (let i = 0; i < data.length; i++) {
+        if (Object.keys(data[i])[0] === dataBaseName) {
+          BDD = data[i][dataBaseName];
+          console.log("BDD", BDD);
+          for (let index = 0; index < BDD.length; index++) {
+            console.log("BD", Object.keys(BDD[index]));
+            if (Object.keys(BDD[index]) == table) {
+              BDD[index][table] = [...BDD[index][table], body];
+            }
+          }
+          writeFile(data);
+        }
+      }
+    } else {
+      setHeader(res, 404);
+      res.end('{ "message": "Must provide a database name"}');
+    }
+  });
+}
+function deleteTable(dataBase, req, res) {
+  let body = [];
+  req.on("data", function (data) {
+    body.push(data);
+  });
+  req.on("end", function () {
+    body = JSON.parse(Buffer.concat(body).toString());
+    if (isDefined(body.name)) {
+      const filtredDataBase = dataBase.filter(
+        (el) => Object.keys(el)[0] !== body.name
+      );
+      writeFile(filtredDataBase);
+    } else {
+      setHeader(res, 404);
+      res.end('{ "message": "Must provide a database name"}');
+    }
+  });
+}
